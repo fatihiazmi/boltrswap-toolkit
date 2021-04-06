6c3cb3e03946b5805278348c7f209a6357719329
nick_f <70769424+fatihiazmi@users.noreply.github.com>
2021-04-06 15:42:52 +0800

    change theme

--- packages/pancake-uikit/src/theme/colors.ts
+++ packages/pancake-uikit/src/theme/colors.ts
@@ -48,7 +48,7 @@ export const darkColors: Colors = {
   tertiary: "#353547",
   text: "#EAE2FC",
   textDisabled: "#666171",
-  textSubtle: "#A28BD4",
+  textSubtle: "#8bb7d4",
   borderColor: "#524B63",
   card: "#2c2629",
   gradients: {
