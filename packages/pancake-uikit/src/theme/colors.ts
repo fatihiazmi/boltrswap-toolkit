29357a09f926d3f8d420c3d716d64f09d11d64cb
nick_f <70769424+fatihiazmi@users.noreply.github.com>
2021-04-06 14:25:30 +0800

    bg & version change

--- packages/pancake-uikit/src/theme/colors.ts
+++ packages/pancake-uikit/src/theme/colors.ts
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#00AACE",
+  background: "#fcfcfc",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
